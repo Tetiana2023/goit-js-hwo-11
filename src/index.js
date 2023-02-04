b63478ac701782d993a830bceace6b334b90d454
Tetiana2023 <drofaj18@gmail.com>
2023-02-04 15:44:03 +0200

    last

--- src/index.js
+++ src/index.js
@@ -101,25 +101,25 @@ function renderGallery(picture) {
     .map(
       ({webformatURL, largeImageURL, tags, likes, views, comments, downloads,
       }) => {
-        return `<a class="gallery__link" href="${largeImageURL}" >
-         <div class="photo-card">
-         <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-         <div class="info">
-          <p class="info-item">
-            <b>Likes ${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>Views ${views}</b>
-          </p>
-          <p class="info-item">
-            <b>Comments ${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>Downloads ${downloads}</b>
-          </p>
-        </div>
-      </div>
-      </a>`;
+        return  `<a class="gallery__link" href="${largeImageURL}" >
+        <div class="photo-card">
+        <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+         <p class="info-item">
+           <b>Likes ${likes}</b>
+         </p>
+         <p class="info-item">
+           <b>Views ${views}</b>
+         </p>
+         <p class="info-item">
+           <b>Comments ${comments}</b>
+         </p>
+         <p class="info-item">
+           <b>Downloads ${downloads}</b>
+         </p>
+       </div>
+     </div>
+     </a>`;
       }
     )
     .join('');
